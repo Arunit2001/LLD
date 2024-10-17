adb9c1cbe8f505f81129ee99db1ca1d7c1f92077
Arunit2001 <uzumaki@launch-pad.local>
2024-10-18 00:21:59 +0530

    Update in ATM lld

--- src/ATM/ATMMachine.java
+++ src/ATM/ATMMachine.java
@@ -3,12 +3,22 @@ package ATM;
 public class ATMMachine {
     static int nextId = 1;
     final int id;
+    double balance;
     ATMState state;
     Card card;
 
     public ATMMachine() {
         this.id = nextId++;
         this.state = new IdleState();
+        this.balance = 3500;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public ATMState getState() {
